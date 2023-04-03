34693493a2a637e1d4a174d6114484c2a78d6e6c
Carlos Berkenbrock <carlosberken7@gmail.com>
2023-04-03 18:43:41 -0300

    100%

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 function toggleMode() {
   const html = document.documentElement
-  html.classList.toggle('light')
+  html.classList.toggle("light")
 
   // pegar a tag img
   const img = document.querySelector("#profile img")
 
   // substituir a imagem
-  if(html.classList.contains('light')){
+  if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
-   img.setAttribute("src", "./assets/avatar-light.png")
-  } else { 
-   img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    img.setAttribute("src", "./assets/Avatar.png")
   }
 
   // se tiver sem light mode, manter a imagem normal
-
-
-  
 }
